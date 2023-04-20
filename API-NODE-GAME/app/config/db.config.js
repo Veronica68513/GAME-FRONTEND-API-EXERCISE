9255c6d2b4115941101a72336076b889faf4cc79
Veronica68513 <veronica.68513@gmail.com>
2023-04-20 15:56:33 +0200

    Update db.config.js
    Updated DB local configuration.

--- API-NODE-GAME/app/config/db.config.js
+++ API-NODE-GAME/app/config/db.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    //url: "mongodb://localhost:27017/veronica"
-    url: "mongodb+srv://veronica68513:syvvo1-pitxop-hUzgyx@cluster0.ngkourb.mongodb.net/games-pro"
+    url: "mongodb://localhost:27017/veronica"
   };
