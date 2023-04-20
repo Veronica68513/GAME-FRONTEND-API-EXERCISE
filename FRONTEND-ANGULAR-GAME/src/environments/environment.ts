c0e468c1b95dbd4b625c0fa960918346437af91a
Veronica68513 <veronica.68513@gmail.com>
2023-04-20 17:19:32 +0200

    MongoDB - Add Environment Variable
    Add environment Variable to connect MongoDB

--- FRONTEND-ANGULAR-GAME/src/environments/environment.ts
+++ FRONTEND-ANGULAR-GAME/src/environments/environment.ts
@@ -1,5 +1,10 @@
 // src/environments/environment.ts
   export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8088/api/games'
+    apiUrl: 'http://localhost:8088/api/games',
+    envVar: {
+      endpointUrl: 'http://localhost:8088/api/games',
+      LOG_LEVEL: 'debug',
+      version: 0
+    }
   }
