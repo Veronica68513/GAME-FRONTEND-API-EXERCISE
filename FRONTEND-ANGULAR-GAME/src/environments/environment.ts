870ece169b867737169ca6c68bf65fcf3c75c48b
Veronica68513 <veronica.68513@gmail.com>
2023-04-21 11:45:33 +0200

    Updated Demo links
    Updated: demo links

--- FRONTEND-ANGULAR-GAME/src/environments/environment.ts
+++ FRONTEND-ANGULAR-GAME/src/environments/environment.ts
@@ -1,9 +1,9 @@
 // src/environments/environment.ts
   export const environment = {
     production: false,
-    apiUrl: 'https://api-node-game.onrender.com',
+    apiUrl: 'https://api-node-game.onrender.com/api/games',
     envVar: {
-      endpointUrl: 'https://api-node-game.onrender.com',
+      endpointUrl: 'https://api-node-game.onrender.com/api/games',
       LOG_LEVEL: 'debug',
       version: 0
     }
